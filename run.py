310b1a2d3a9be75acb733c3efafec7a8294d28fc
tkml87 <>
2023-08-17 03:46:40 +0000

    add some features to prevent bugs

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 # Conception:
 # Ask user's name
+# - Avoid user from put non-alphabetical characters and a name of any length
+# - Ask for user to start the game by him/herserlf
 # Load questions
 # Receive answers and value them
+# - Refrain user from put anything as answer; Accpecting A, B, C or D as answer
 # Finish the quiz informing the score and asking about restart
